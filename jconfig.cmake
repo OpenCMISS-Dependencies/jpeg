4390b04c27d4c41962a8469f147572babc66244c
hsorby <h.sorby@auckland.ac.nz>
2020-11-18 11:44:13 +1300

    Add binary directory to include path for executables.  Correct source file names.

--- jconfig.cmake
+++ jconfig.cmake
@@ -16,10 +16,18 @@
 #cmakedefine INCOMPLETE_TYPES_BROKEN
 
 /* Define "boolean" as unsigned char, not int, per Windows custom */
+#ifdef _WIN32
 #ifndef __RPCNDR_H__		/* don't conflict if rpcndr.h already read */
 typedef unsigned char boolean;
 #endif
+#ifndef FALSE			/* in case these macros already exist */
+#define FALSE	0		/* values of boolean */
+#endif
+#ifndef TRUE
+#define TRUE	1
+#endif
 #define HAVE_BOOLEAN		/* prevent jmorecfg.h from redefining it */
+#endif
 
 
 #ifdef JPEG_INTERNALS
